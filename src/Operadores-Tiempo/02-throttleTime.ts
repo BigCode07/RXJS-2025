a74cd16ad0f4fc7694c4fc14fddd816b9bcde0a7
bigcode <bigcode07@gmail.com>
2025-05-05 06:40:36 -0300

    feat: TimeOperator ThrottleTime

---
+++ src/Operadores-Tiempo/02-throttleTime.ts
@@ -0,0 +1,22 @@
+import { distinctUntilChanged, throttleTime } from "rxjs/operators";
+import { asyncScheduler, fromEvent, map } from "rxjs";
+
+const click$ = fromEvent(document, "click");
+
+click$.pipe(throttleTime(3000)).subscribe(console.log);
+
+const input = document.createElement("input");
+document.querySelector("body").append(input);
+
+const input$ = fromEvent<KeyboardEvent>(input, "keyup");
+
+input$
+  .pipe(
+    throttleTime(1000, asyncScheduler, {
+      leading: true,
+      trailing: true,
+    }),
+    map((event) => (event.target as HTMLInputElement).value),
+    distinctUntilChanged()
+  )
+  .subscribe(console.log);
